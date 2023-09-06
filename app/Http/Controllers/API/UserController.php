88ddb33e1c23050d70d9750b313b8c18c89db054
Lois Bassey <loisbassey@gmail.com>
2023-09-06 23:03:58 +0100

    Removed unnecessary methods and populated the controller with needed methods to get finished tasks by merchant

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,22 +3,34 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\GetMerchantFinishedTasksRequest;
+use App\Http\Responses\APIResponse;
+use App\Http\Services\TaskService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function profile()
-    {
+    /** @var TaskService $taskService */
+    private TaskService $taskService;
 
+    /** @var APIResponse $response */
+    private APIResponse $response;
+
+    public function __construct()
+    {
+        $this->taskService = new TaskService();
+        $this->response = new APIResponse();
     }
 
-    public function edit()
+    /**
+     * @param string $uuid
+     * @param GetMerchantFinishedTasksRequest $request
+     * @return JsonResponse
+     */
+    public function finishedTasksByMerchant(string $uuid, GetMerchantFinishedTasksRequest $request): JsonResponse
     {
-
-    }
-
-    public function delete()
-    {
-
+        $tasks = $this->taskService->listByUser($uuid);
+        return $this->response->setData($tasks)->success();
     }
 }
