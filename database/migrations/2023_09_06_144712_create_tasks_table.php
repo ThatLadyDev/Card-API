5baa17c1ad05ea360726e08394336bc417eb79f0
Lois Bassey <loisbassey@gmail.com>
2023-09-06 23:02:52 +0100

    Made updates to the migration file for the tasks table.

--- database/migrations/2023_09_06_144712_create_tasks_table.php
+++ database/migrations/2023_09_06_144712_create_tasks_table.php
@@ -14,15 +14,13 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->uuid();
-            $table->unsignedBigInteger('card_id');
-            $table->unsignedBigInteger('merchant_id');
+            $table->unsignedBigInteger('user_id');
             $table->string('type');
             $table->boolean('is_finished')->default(false);
             $table->string('status')->default('in progress');
             $table->timestamps();
 
-            $table->foreign('card_id')->references('id')->on('cards');
-            $table->foreign('merchant_id')->references('id')->on('merchants');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
