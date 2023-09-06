31d5e3d554f2fe1b66e132f1a09e543f5ec62eb8
Lois Bassey <loisbassey@gmail.com>
2023-09-06 14:42:57 +0100

    Created needed migration files for the application

---
+++ database/migrations/2023_09_06_124343_create_cards_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->uuid();
+            $table->unsignedBigInteger('user_id');
+            $table->string('card_number')->unique();
+            $table->date('expiration_date');
+            $table->string('cvv');
+            $table->string('cardholder_name');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cards');
+    }
+};
