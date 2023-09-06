7afaf50cc097625035269778a7628c588ab8e788
Lois Bassey <loisbassey@gmail.com>
2023-09-06 23:06:18 +0100

    Modifications and feature updates

--- app/Http/Services/TaskService.php
+++ app/Http/Services/TaskService.php
@@ -4,6 +4,7 @@ namespace App\Http\Services;
 
 use App\Exceptions\APIException;
 use App\Http\Requests\Task\CreateRequest;
+use App\Models\CardSwitch;
 use App\Models\Task;
 use Exception;
 use Illuminate\Support\Str;
@@ -16,6 +17,9 @@ class TaskService
     /** @var CardService $cardService */
     private CardService $cardService;
 
+    /** @var UserService $userService */
+    private UserService $userService;
+
     /** @var Task $task */
     private Task $task;
 
@@ -23,6 +27,56 @@ class TaskService
     {
         $this->merchantService = new MerchantService();
         $this->cardService = new CardService();
+        $this->userService = new UserService();
+    }
+
+    /**
+     * @param string $uuid
+     * @return array
+     */
+    public function listByUser(string $uuid): array
+    {
+        $userId = $this->userService->get($uuid, ['id'])['id'];
+        $tasks = Task::query()
+            ->select(
+                'tasks.id',
+                'cards.card_number',
+                'cards.expiration_date',
+                'cards.cvv',
+                'cards.cardholder_name',
+                'merchants.name as merchant_name',
+                'tasks.type as task_type',
+                'card_switches.merchant_id'
+            )
+            ->join('card_switches', 'card_switches.task_id', '=', 'tasks.id')
+            ->join('cards', 'card_switches.new_card_id', '=', 'cards.id')
+            ->join('merchants', 'card_switches.merchant_id', '=', 'merchants.id')
+            ->where('tasks.user_id', $userId)
+            ->where('tasks.is_finished', true)
+            ->latest('tasks.created_at')
+            ->get();
+
+        return $tasks->groupBy('merchant_id')->toArray();
+    }
+
+    /**
+     * @param CreateRequest $request
+     * @param int $taskId
+     * @return void
+     */
+    private function switchCards(CreateRequest $request, int $taskId): void
+    {
+        $previousCardId = $this->cardService->get($request->previousCardUuid, ['id'])['id'];
+        $newCardId = $this->cardService->get($request->newCardUuid, ['id'])['id'];
+        $merchantId = $this->merchantService->get($request->merchantUuid, ['id'])['id'];
+
+        CardSwitch::query()->create([
+            'uuid' => Str::uuid(),
+            'task_id' => $taskId,
+            'new_card_id' => $newCardId,
+            'previous_card_id' => $previousCardId,
+            'merchant_id' => $merchantId,
+        ]);
     }
 
     /**
@@ -33,15 +87,15 @@ class TaskService
     public function create(CreateRequest $request): void
     {
         try {
-            $cardId = $this->cardService->get($request->cardUuid, ['id'])['id'];
-            $merchantId = $this->merchantService->get($request->merchantUuid, ['id'])['id'];
-
-            Task::query()->create([
+            $task = Task::query()->create([
+                'user_id' => auth()->id(),
                 'uuid' => Str::uuid(),
-                'card_id' => $cardId,
-                'merchant_id' => $merchantId,
                 'type' => $request->type
             ]);
+
+            if ($request->type === 'card_switch'){
+                $this->switchCards($request, $task->id);
+            }
         }
         catch (Exception $e){
             throw new APIException($e->getMessage());
