42c4a93c1920f45f62b3f8ce601dda2bd4c05993
Lois Bassey <loisbassey@gmail.com>
2023-09-06 14:58:16 +0100

    Added two columns: uuid, is_super_admin to the migration file for users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->uuid();
             $table->string('name');
             $table->string('email')->unique();
+            $table->boolean('is_super_admin')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
