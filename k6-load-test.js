e4c605676a6bc3e9d4c09656bfe7ff3e33f831b9
Lois Bassey <loisbassey@gmail.com>
2023-09-07 21:50:13 +0100

    Application updates

--- k6-load-test.js
+++ k6-load-test.js
@@ -2,9 +2,10 @@
 import http from "k6/http";
 
 export const options = {
-    iterations: 15,
+    iterations: 100,
+    // duration: 1000
 };
 
 export default function () {
-    const response = http.get("https://codevixens.org");
+    const response = http.get("https://project-knot.loisbassey.com");
 }
